a29949a8f91a745038ae0207e0f302f1d4b993ad
Seraph-YCZhang <zyc199685@outlook.com>
2020-06-01 22:33:18 -0400

    implement firebase utils, sign up

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-up.component';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -16,9 +17,26 @@ class App extends React.Component {
   }
   unsubscribeFromAuth = null
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser:{
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          },()=>{
+            console.log(this.state);
+          });
+        });
+        
+      }else {
+        this.setState({
+          currentUser: userAuth
+        });
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -28,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
