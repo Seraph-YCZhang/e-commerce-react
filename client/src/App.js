289e605632ae246a7482cb3093d0600c3bed9ed7
Seraph-YCZhang <zyc199685@outlook.com>
2020-07-03 23:26:02 -0400

    some changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-up.component';
 import CheckOutPage from './pages/checkout/checkout.component'
+
+import { GlobalStyle } from './global.styles';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
@@ -19,6 +21,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
   return (
     <div>
+      <GlobalStyle />
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
